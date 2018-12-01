2b23e50b1fb1ee266372d71fbcc337679b9d8274
Florian Goeteyn <florian@chapps.com>
2018-12-01 12:43:03 +0100

    Support lib projects Xamarin and Android

---
+++ SupportLib/SupportLibXamarin/App.Droid/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+
+namespace App.Droid
+{
+    [Activity(Label = "SupportLibXamarin", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, View.IOnClickListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.SetOnClickListener(this);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            var id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+        
+        public void OnClick(View v)
+        {
+            if (v.Id == Resource.Id.fab)
+            {
+                Snackbar.Make(v, "Replace with your own action", Snackbar.LengthLong)
+                    .SetAction("Action", listener:null).Show();
+            }
+        }
+    }
+}
+
